bb6b63ac6ca5323b4cc3419107e1d25d0679f25f
Leon <malkavianleon@gmail.com>
2023-05-16 14:25:16 -0300

    Refatorado o código e também adicionado funções para permitir que os botões e os placeholders trocassem de informação conforme o código fosse gerado

--- script.js
+++ script.js
@@ -1,60 +1,43 @@
-//variáveis
+// Variáveis
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const button = document.getElementById("random");
+const btnvalue1 = document.getElementById("btnvalue1")
+const btnvalue2 = document.getElementById("btnvalue2")
+const place1 = document.getElementById("place1")
+const place2 = document.getElementById("place2")
 
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var btn1 = document.querySelector(".btn1");
-var btn2 = document.querySelector(".btn2");
-var button = document.getElementById("random");
-
-//funções
-
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
+// Funções
+const setGradient = () => {
+	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
 	css.textContent = body.style.background + ";";
 };
 
-function setGradientkey() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ btn1.value 
-	+ ", " 
-	+ btn2.value 
-	+ ")";
-
+const setGradientkey = () => {
+	body.style.background = `linear-gradient(to right, ${btn1.value}, ${btn2.value})`;
 	css.textContent = body.style.background + ";";
 };
 
-function randomButton() {
-	var conta1 = Math.floor(Math.random() * 100000000000000000)
-	var conta2 = Math.floor(Math.random() * 100000000000000000)
-	var btconta1 = "#" + ("000000" + conta1).slice(-6);
-	var btconta2 = "#" + ("000000" + conta2).slice(-6);
-	body.style.background =
-	"linear-gradient(to right, " 
-	+ btconta1 
-	+ ", " 
-	+ btconta2 
-	+ ")";
+const randomButton = () => {
+	const conta1 = Math.floor(Math.random() * 100000000000000000);
+	const conta2 = Math.floor(Math.random() * 100000000000000000);
+	const btconta1 = "#" + ("000000" + conta1).slice(-6);
+	const btconta2 = "#" + ("000000" + conta2).slice(-6);
+	body.style.background = `linear-gradient(to right, ${btconta1}, ${btconta2})`;
 	css.textContent = body.style.background + ";";
+	btnvalue1.value = btconta1
+	btnvalue2.value = btconta2
+	place1.setAttribute("placeholder", btconta1)
+	place2.setAttribute("placeholder", btconta2)
 };
 
-//code
-
+// Event Listeners
 button.addEventListener("click", randomButton);
-
 btn1.addEventListener("input", setGradientkey);
-
 btn2.addEventListener("input", setGradientkey);
-
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
-
